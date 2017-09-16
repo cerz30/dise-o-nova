498400d9032ecb3bd2eb1013ede07af1bae54b52
cerz <rodriguezzcarlose@gmail.com>
2017-09-16 11:21:11 -0400

    modificacion de scrip.js

--- plugins.js
+++ plugins.js
@@ -1,4 +1,2 @@
 console.log ('Soy plugins');
-var mensaje ="sdadasdasdas"
-var suma = 5+5;
-var resta = 5-3;
+
--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 console.log('soy script');
 var mensaje ="dasfgasdasd";
+var mensaje2 ="sdadasdasdas"
+var suma = 5+5;
+var resta = 5-3;
